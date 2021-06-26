c49d7d7607b84227cc42390858d696980f0998ab
chris-ibeh <85839629+chris-ibeh@users.noreply.github.com>
2021-06-26 23:03:50 +0100

    ProjectStudent
    Accept student informations

--- Program.cs
+++ Program.cs
@@ -1,40 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace PrimeOddEvenNumber
+namespace ProjectStudent
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int numb, x, y = 0;
-            Console.Write("Enter any Number: ");
-            numb = int.Parse(Console.ReadLine());
+            //Console.WriteLine("How Many Students are in your class?");
+            //var studentCount = int.Parse(Console.ReadLine());
 
-            for (x = 2; x <= numb / 2; x++)
+            //var studentNames = new String [studentCount];
+            //var studentGrades = new int[studentCount];
+            var students = new List<Student>();
+
+            ////var studentNames = new List<string>();
+            ////var studentGrades = new List<int>();
+
+            //for (int x = 0; x < studentCount; x++)
+
+            var adding = true;
+
+            while (adding)
             {
-                if (numb % x == 0)
-                {
-                    Console.Write("FIZZBUZZ the Number you typed is not a Prime Number");
-                    y = 1;
-                    break;
-                }
+                var newstudent = new Student();
+
+                Console.WriteLine("What is your student Name ");
+                newstudent.Name = Console.ReadLine();
+
+                Console.WriteLine("What is your student Grade ");
+                newstudent.Grade = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("What is your student Birthday ");
+                newstudent.Birthday = Console.ReadLine();
+
+                Console.WriteLine("What is your student Address ");
+                newstudent.Address = Console.ReadLine();
+
+                Console.WriteLine("What is your student Phone");
+                newstudent.Phone = int.Parse(Console.ReadLine());
+
+                students.Add(newstudent);
+
+                Console.WriteLine("Add new student? y/n");
+                if (Console.ReadLine() != "y")
+                    adding = false;
             }
-            if (y == 0)
+
+            foreach (var student in students)
             {
-                Console.Write("FIZZBUZZ the Number you typed is a Prime Number");
-            }
-                Console.ReadLine();
-            
-            if (numb % 2 == 0)
-            {
-                Console.WriteLine("BUT FIZZ The Number you typed is also Even Number ");
-                
-            }
-            else if (numb % 2 != 0)
-            {
-                Console.WriteLine("BUT BUZZ The Number you typed is also Odd Number ");
-                
+                Console.WriteLine("Your Name is: {0} , Your Grade is {1}", student.Name, student.Grade);
             }
         }
+    }
+    class Student
+    {
+        public string Name;
+        public int Grade;
+        public string Birthday;
+        public string Address;
+        public int Phone;
     }
 }
