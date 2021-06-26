9440aa1ef9e497b96388a07b6726d3078446bc41
chris-ibeh <85839629+chris-ibeh@users.noreply.github.com>
2021-06-26 22:27:05 +0100

    PrimeNumbers
    Check for Prime, Odd & Even Numbers

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PrimeOddEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb, x, y = 0;
+            Console.Write("Enter any Number: ");
+            numb = int.Parse(Console.ReadLine());
+
+            for (x = 2; x <= numb / 2; x++)
+            {
+                if (numb % x == 0)
+                {
+                    Console.Write("FIZZBUZZ the Number you typed is not a Prime Number");
+                    y = 1;
+                    break;
+                }
+            }
+            if (y == 0)
+            {
+                Console.Write("FIZZBUZZ the Number you typed is a Prime Number");
+            }
+                Console.ReadLine();
+            
+            if (numb % 2 == 0)
+            {
+                Console.WriteLine("BUT FIZZ The Number you typed is also Even Number ");
+                
+            }
+            else if (numb % 2 != 0)
+            {
+                Console.WriteLine("BUT BUZZ The Number you typed is also Odd Number ");
+                
+            }
+        }
+    }
+}
